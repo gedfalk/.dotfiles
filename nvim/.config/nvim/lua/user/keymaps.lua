69f59f6f00982e8333bbb217b37d5eab7a91e3fa
gedfalk <blite.co.stn@gmail.com>
2024-08-19 19:11:33 +0300

    Switching my config to lua

---
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,27 @@
+-- Variables
+local opts = { noremap = true, silent = true }
+local nv_modes = { 'n', 'v' }
+local iv_modes = { 'i', 'v' }
+local ivx_modes = { 'i', 'v', 'x' }
+
+-- Change default navigation keys
+vim.keymap.set(nv_modes, 'k', '<Down>', opts)
+vim.keymap.set(nv_modes, 'j', '<Left>', opts)
+vim.keymap.set(nv_modes, 'l', '<Right>', opts)
+vim.keymap.set(nv_modes, 'i', '<Up>', opts)
+vim.keymap.set(nv_modes, 'h', '<insert>', opts)
+
+-- Change <ESC> to 'zz'
+vim.keymap.set(ivx_modes, 'zz', '<ESC>', opts)
+
+-- Tab several times
+vim.keymap.set('v', '>', '>gv', opts)                 
+vim.keymap.set('v', '<', '<gv', opts)                  
+
+-- Move text up and down
+vim.keymap.set(nv_modes, '<C-k>', ':m .+1<CR>==')
+vim.keymap.set(nv_modes, '<C-i>', ':m .-2<CR>==')
+
+-- Move text lines up and down
+vim.keymap.set("x", "<C-k>", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("x", "<C-i>", ":m '<-2<CR>gv=gv", opts)
---
+++ nvim/.config/nvim/lua/user/options.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true                   -- enable line numbers
+vim.opt.tabstop = 4                     -- number of spaces in <Tab>
+vim.opt.shiftwidth = 4                  -- size of an indent
+vim.opt.expandtab = true                -- use spaces instead of tabs
+vim.opt.clipboard = "unnamedplus"       -- use system clipboard
+vim.opt.ignorecase = true               -- ignore case while searching
+
+vim.opt.scrolloff = 8                   -- scrolling 8 lines before edge
+vim.opt.sidescrolloff = 8               -- same, but horizontally
+                                                                        
+-- vim.opt.colorcolumn = '80'           TODO: change color
+
+-- vim.opt.cursorline = true            TODO: is it customizable            
