d6f6329498e6ba11aa38ef09c145d9ecca50d34e
gedfalk <blite.co.stn@gmail.com>
2024-09-02 19:04:59 +0300

    Refactor: changing key-bindings, notifying...
    - Changing key-modes
    - Turning off annoying notifyin every time neovim is launced
    - Adding transparency

--- nvim/.config/nvim/lua/plugins/all.lua
+++ nvim/.config/nvim/lua/plugins/all.lua
@@ -1,6 +1,9 @@
 return {
     -- Themes
-    {"catppuccin/nvim", name = "catppuccin", priority = 1000},
+    {"catppuccin/nvim", name = "catppuccin", priority = 1000,
+        opts = {
+            transparent_background = true}
+    },
     {"folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {}},
     
     -- Stuff
--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,27 +1,29 @@
 -- Variables
 local opts = { noremap = true, silent = true }
-local nv_modes = { 'n', 'v' }
-local iv_modes = { 'i', 'v' }
-local ivx_modes = { 'i', 'v', 'x' }
-local nivx_modes = { 'n', 'i', 'v', 'x' }
+local nv = { 'n', 'v' }
+local iv = { 'i', 'v' }
+local ivx = { 'i', 'v', 'x' }
+local nivx = { 'n', 'i', 'v', 'x' }
 
 -- Change default navigation keys
-vim.keymap.set(nv_modes, 'k', '<Down>', opts)
-vim.keymap.set(nv_modes, 'j', '<Left>', opts)
-vim.keymap.set(nv_modes, 'l', '<Right>', opts)
-vim.keymap.set(nv_modes, 'i', '<Up>', opts)
-vim.keymap.set(nv_modes, 'h', '<insert>', opts)
+vim.keymap.set(nv, 'l', '<Right>', opts)
+vim.keymap.set(nv, 'i', '<Up>', opts)
+vim.keymap.set(nv, 'k', '<Down>', opts)
+vim.keymap.set(nv, 'j', '<Left>', opts)
+vim.keymap.set(nv, 'h', '<insert>', opts)
 
 -- Change <ESC> to 'zz'
-vim.keymap.set(ivx_modes, 'zz', '<ESC>', opts)
+vim.keymap.set(ivx, 'zz', '<ESC>', opts)
 
 -- Tab several times
-vim.keymap.set('v', '>', '>gv', opts)                 
-vim.keymap.set('v', '<', '<gv', opts)                  
+vim.keymap.set('v', '<C-l>', '>gv', opts)                 
+vim.keymap.set('v', '<C-j>', '<gv', opts)
+vim.keymap.set('n', '<C-l>', '>>', opts)
+vim.keymap.set('n', '<C-j>', '<<', opts)
 
 -- Move text up and down
-vim.keymap.set(nv_modes, '<C-k>', ':m .+1<CR>==')
-vim.keymap.set(nv_modes, '<C-i>', ':m .-2<CR>==')
+vim.keymap.set(nv, '<C-k>', ':m .+1<CR>==')
+vim.keymap.set(nv, '<C-i>', ':m .-2<CR>==')
 
 -- Move text lines up and down
 vim.keymap.set("x", "<C-k>", ":m '>+1<CR>gv=gv", opts)
@@ -30,4 +32,4 @@ vim.keymap.set("x", "<C-i>", ":m '<-2<CR>gv=gv", opts)
 -- Plugins
     -- Neo-tree
     -- Open Neotree on Ctrl-e
-    vim.keymap.set(nivx_modes, '<C-e>', ':Neotree<CR>', opts)
+    vim.keymap.set(nivx, '<C-e>', ':Neotree<CR>', opts)
