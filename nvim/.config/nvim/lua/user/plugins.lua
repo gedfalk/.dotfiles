b08b5707536159f538df95486d1c2e93cc469985
gedfalk <gedfalk@blite.co.stn@gmail.com>
2025-06-04 01:36:26 +0300

    feat(nvim): set different colorschemes for different types of files

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,9 +1,18 @@
 --- Not completely sure... it's like some additional plugin options I cannot put in ~/.dotfiles/nvim/.config/nvim/lua/plugins
 
+---
 
 -- For example, although I have these themes in .../plugins folder, they wouldn't work by themselves, if i don't activate them here
---vim.cmd[[colorscheme tokyonight]]
-vim.cmd.colorscheme "catppuccin"
+-- Set default colorscheme
+vim.cmd.colorscheme('catppuccin') 
+
+-- Change colorscheme for Markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.cmd.colorscheme('tokyonight')
+  end,
+})
 
 
 -- And this is fix for render-markdown, because it throws some mistakes on my treesitter
