a0f358b3b2b4f814accd312629782d525c3ba708
gedfalk <gedfalk@blite.co.stn@gmail.com>
2025-06-04 01:29:50 +0300

    config(nvim): tweaking render-markdown

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,26 +1,31 @@
 return {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {
-        heading = {
-            border = true,
-        },
-        code = {
-            width = "block",
-            left_pad = 2,
-            right_pad = 4,
-            border = "thick",
-            position = "right",
-        },
-        -- TODO: change checkboxes
-        checkbox = {
-            -- unchecked = { icon = "✘ " },
-            -- checked = { icon = "✔ " },
-            -- custom = { todo = { rendered = "◯ " } },
-        },
-    },
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {
+		heading = {
+			border = true,
+		},
+		code = {
+			width = "block",
+			left_pad = 2,
+			right_pad = 4,
+			border = "thick",
+			position = "right",
+		},
+		-- TODO: change checkboxes
+		checkbox = {
+			-- unchecked = { icon = "✘ " },
+			-- checked = { icon = "✔ " },
+			-- custom = { todo = { rendered = "◯ " } },
+		},
+		dash = {
+			highlight = "RenderMarkdownH5fg",
+		},
+        bullet = {
+        highlight = "RenderMarkdownH1fg"}
+	},
 }
